f6ad2fb301c60a0224457315d590df21a863493e
Jesse Markowitz <jess.markowitz@gmail.com>
2020-03-22 13:21:09 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -1,6 +1,2 @@
 var chocolateBars = ["snickers", "hundred grand", "kitkat", "skittles"];
 
-function addElementToBeginningOfArray(Array, "x") {
-  const newArray = ["x", ...Array]
-  return newArray
-}
